6769ba2a6814e38ba8802328091e0ff36e3397a1
Princewell <r0833920@student.thomasmore.be>
2022-12-11 19:19:49 +0100

    Bug fixes

--- detect.py
+++ detect.py
@@ -68,7 +68,7 @@ def detect(
         visualize=False,  # visualize features
         update=False,  # update all models
         project=ROOT / 'runs/detect',  # save results to project/name
-        name='cum',  # save results to project/name
+        name='exp',  # save results to project/name
         exist_ok=False,  # existing project/name ok, do not increment
         line_thickness=3,  # bounding box thickness (pixels)
         hide_labels=False,  # hide labels
@@ -87,15 +87,10 @@ def detect(
         source = check_file(source)  # download
 
     # Directories
-<<<<<<< HEAD
+
     #save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
     #(save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
     save_dir = Path(project / name)
-=======
-    save_dir = Path(project / name)
-    #save_dir = increment_path(Path(project) / name, exist_ok=exist_ok)  # increment run
-    (save_dir / 'labels' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
->>>>>>> 4921aa4f6a64b2c0dbd35a30def3798f9a0f7286
 
     # Load model
     device = select_device(device)
